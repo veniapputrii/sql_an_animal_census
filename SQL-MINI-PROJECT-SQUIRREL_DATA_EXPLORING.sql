f892c21fb780432fa09f21cc1412f96428358b36
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-06-27 11:30:40 +0700

    Add files via upload

--- SQL-MINI-PROJECT-SQUIRREL_DATA_EXPLORING.sql
+++ SQL-MINI-PROJECT-SQUIRREL_DATA_EXPLORING.sql
@@ -1,5 +1,8 @@
--- 1. Throw away the table with the missing slot
-DROP TABLE IF EXISTS squirrel_data;
+
+
+
+
+begin;
 
 -- 2. Build the 16-slot container
 CREATE TABLE squirrel_data (
@@ -22,26 +25,63 @@ CREATE TABLE squirrel_data (
 );
 
 -- 3. Suck the file in
-COPY squirrel_data 
-FROM '/tmp/squirrel-data.csv' 
-WITH (FORMAT csv, HEADER true, ENCODING 'WIN1252');
-SELECT  *
-FROM squirrel_data 
-LIMIT 10;
+COPY squirrel_data FROM '/Users/heidi/Downloads/squirrel-data.csv' WITH (FORMAT csv);
+select * from squirrel_data;
+rollback;
 
--- ✨ DELETE the null values --
-delete from squirrel_data
-Where area_name is null
-	or park_name is null
-	or squirrel_id is null
-	or primary_fur_color is null
-	or highlights_in_fur_color is null
-	or color_notes is null
-	or location is null
-	or above_ground_height_feet is null
-	or spesific_location  is null
-	or activities is null
-	or interactions_with_humans is null
-	or other_notes_or_observations is null
-	or latitude is null 
-	or longatitude is null;
+--✨ Filtering location
+Select
+	park_name,
+	count(DISTINCT squirrel_id) AS total_squirrels
+FROM squirrel_data
+WHERE park_name is not null
+group by park_name
+order by total_squirrels DESC;
+
+--✨ Common Activity Trend
+select * from squirrel_data;
+--use ILIKE for a sensitive value like Foraging, Nesting/gathering leaves
+CREATE TEMP TABLE temp_common as
+SELECT
+	COUNT(*) FILTER(WHERE activities ILIKE '%Running%') AS Running_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Chasing') AS Chasing_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Foraging%') AS Foraging_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Nesting%') AS Nesting_activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Digging%') AS Digging_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Climbing%') AS Climbing_activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Eating%') AS Eating_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Defending%') AS Defending_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%shouting%') AS Shouting_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Grooming%') AS Grooming_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Sitting%') AS Sitting_Activities,
+	COUNT (*) FILTER (WHERE activities ILIKE '%Lounging%') AS Lounging_Activities
+From squirrel_data;
+Select * From temp_common;
+Begin;
+
+
+SELECT 'Foraging' AS activities_name, COUNT(*) FILTER (WHERE foraging_activities = true) AS total FROM temp_common
+UNION ALL
+SELECT 'Nesting', COUNT(*) FILTER (WHERE nesting_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Digging', COUNT(*) FILTER (WHERE digging_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Climbing', COUNT(*) FILTER (WHERE climbing_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Eating', COUNT(*) FILTER (WHERE eating_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Defending', COUNT(*) FILTER (WHERE defending_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Shouting', COUNT(*) FILTER (WHERE shouting_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Grooming', COUNT(*) FILTER (WHERE grooming_activities = true) FROM squirrel_data
+
+UNION ALL
+SELECT 'Running', COUNT(*) FILTER (WHERE running_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'chasing', COUNT(*) FILTER (WHERE chasing_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Sitting', COUNT(*) FILTER (WHERE sitting_activities = true) FROM squirrel_data
+UNION ALL
+SELECT 'Lounging', COUNT(*) FILTER (WHERE lounging_activities = true) FROM squirrel_data
+ORDER BY total DESC;
